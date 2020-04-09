14e0dedfe7d93fce297be2a80e6af33d002d64c0
Joseph Tawil <jtawil2015@gmail.com>
2020-04-08 19:25:09 -0700

    added arrayLoops file and Superchallenge file

---
+++ README.md
@@ -0,0 +1,11 @@
+### File
+
+* [`array-logs-unsolved`](Unsolved/array-logs-unsolved.html)
+
+### Instructions
+
+* Follow the instructions provided in the file to `console.log` each of the names in the `coolPeople` variable.
+
+* **HINT:** You should be repeating the same line 6 times.
+
+- in arrayLoops.js file I start using for loops
---
+++ arrayLoops.js
@@ -0,0 +1,11 @@
+var array = ["A string", 5, 'T',["another",5,0], true,false];
+
+//for loop is saying for a variable index that is set to 0
+// index is less than array length
+//every loop index is added by 1.
+for(var index = 0; index < array.length;index++)
+{
+    // console.log("the index is:", index);
+    // console.log("hello");
+    console.log(array[index]);
+}
